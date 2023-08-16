1d47bd1e24718348c50d5860a5346f4f7d4d0ec1
bengris32 <bengris32@protonmail.ch>
2023-08-16 18:20:00 +0100

    drivers: connectivity: Disable WLAN boost if !CONFIG_MTK_CPU_CTRL
    Change-Id: I4bf1df6b600e2a3c3495e1a149a993cf029c57fa
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/plat/mt6768/plat_priv.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/plat/mt6768/plat_priv.c
@@ -49,6 +49,8 @@ uint32_t kalGetCpuBoostThreshold(void)
 	return 3;
 }
 
+#if KERNEL_VERSION(5, 4, 0) <= CFG80211_VERSION_CODE || !defined(CONFIG_MTK_CPU_CTRL)
+#else
 int32_t kalBoostCpu(IN struct ADAPTER *prAdapter,
 		    IN uint32_t u4TarPerfLevel,
 		    IN uint32_t u4BoostCpuTh)
@@ -88,6 +90,7 @@ int32_t kalBoostCpu(IN struct ADAPTER *prAdapter,
 #endif
 	return 0;
 }
+#endif
 
 #ifdef CONFIG_WLAN_MTK_EMI
 void kalSetEmiMpuProtection(phys_addr_t emiPhyBase, bool enable)
